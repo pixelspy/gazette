26bc91426a4df0fe26681db53cbc163c9079d092
Maxime <lefevre.maxime96@gmail.com>
2017-04-11 13:12:11 +0200

    Version debug content

---
+++ content.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO article (art_id, art_title, art_content)
+ VALUES
+ (1, 'First article', 'Hi, there! This is the very first article.' )
+
+
+
+INSERT INTO users (usr_login, usr_mail, usr_news, usr_news)
+ VALUES
+ ('BucKz', 'lefevre.maxime96@gmail.com', 1, 1)
+ 
