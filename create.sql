a9716e9baa9d5a9126d5091a05c6030effeaa5fe
Maxime <lefevre.maxime96@gmail.com>
2017-04-11 12:37:18 +0200

    First version

---
+++ create.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE if not exists gazette2 character set utf8 collate utf8_unicode_ci;
+
+use gazette2;
---
+++ structure.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE IF NOT EXISTS 'article' (
+
+art_id integer not null primary key auto_increment,
+
+art_title varchar(100) not null,
+
+art_content varchar(2000) not null,
+
+art_date datetime not null,
+
+art_like int(11) not null,
+
+art_dislike int(11) not null,
+
+art_public tinyint(11) not null
+
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` ( 
+
+usr_id integer not null primary key auto_increment,
+
+usr_login varchar(50) not null,
+
+usr_mail varchar(256) not null,
+
+usr_psswd varchar(50) not null,
+
+usr_news tinyint(1) not null, 
+
+usr_admin tinyint(1) not null
+
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+ 
+
