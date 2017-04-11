10d8aca069d9d27c0beede36e3d0f14ddc400f38
Maxime <lefevre.maxime96@gmail.com>
2017-04-11 15:17:18 +0200

    last modif

--- content.sql
+++ content.sql
@@ -1,18 +1,18 @@
 
 
-INSERT INTO article (art_id, art_title, art_content)
+INSERT INTO article (art_title, art_content)
  VALUES
- (1, 'First article', 'Hi, there! This is the very first article.' )
+ ('First article', 'Hi, there! This is the very first article.');
 
  INSERT INTO article (art_title, art_content)
  VALUES 
- ('Second article', 'hey ! Second article, have fun!' )
+ ('Second article', 'hey ! Second article, have fun!' );
 
 
 
 INSERT INTO users (usr_login, usr_mail, usr_news, usr_admin)
  VALUES
- ('BucKz', 'lefevre.maxime96@gmail.com', 1, 1)
+ ('BucKz', 'lefevre.maxime96@gmail.com', 1, 1);
 
 
  
